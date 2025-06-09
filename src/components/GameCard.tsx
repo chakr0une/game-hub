9c4c1839d3cc65cd1541cf1c3f76230e5dae07dc
chakr0une <soufianbenchakroune7@gmail.com>
2025-06-09 05:26:07 +0100

    Cleaning Up the Game Cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -13,13 +13,13 @@ function GameCard({ game }: GameCartProps) {
     <Card>
       <Image src={getCroppesImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize={'2xl'}>{game.name}</Heading>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList
             platform={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScor score={game.metacritic} />
         </HStack>
+        <Heading fontSize={'2xl'}>{game.name}</Heading>
       </CardBody>
     </Card>
   );
